a3d7152f1ea5f4070b8fffbe024ef514da67a612
Firemoon777 <firemoon@icloud.com>
2016-12-09 18:47:46 +0300

    clocking with fifo

--- gravity.sed
+++ gravity.sed
@@ -1,1 +1,2 @@
-#!/bin/gsed
+#!/bin/sed -nf
+/t/d
--- play.sh
+++ play.sh
@@ -1,2 +1,32 @@
 #!/bin/sh
+SLEEP=sleep
+TIME=0.5
+
+# Creating fifo for input
+rm -f gravity-fifo;
+mkfifo gravity-fifo;
+
+# Prevent closing after echo
+sleep 99999999 > gravity-fifo &
+
+# clock
+while true
+do 
+	echo t > gravity-fifo
+       	$SLEEP $TIME 
+done &
+
+# run game
+sed -f gravity.sed gravity-fifo &
+
+# Pass key press to game
+(while true 
+do
+	read -s -n 1 key 
+	echo '>'$key'<'
+done ) | sed -u -e '/t/d' > gravity-fifo
+
+# finish game
+pkill -P $$
+
 
